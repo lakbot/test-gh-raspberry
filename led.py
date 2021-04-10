e20f610d7759b9713f77c7f900e24a82d4753425
Pablo <pabluc@gmail.com>
2021-04-10 18:16:22 -0300

    Change Color

--- led.py
+++ led.py
@@ -3,8 +3,8 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-pinout = 18
-color = "Green"
+pinout = 23
+color = "Yellow"
 
 GPIO.setup(pinout,GPIO.OUT)
 print "LED on N" + str(pinout) + " " + color
